ad070f18a1768d51a45791e06fbf01966265cf0d
HuQingyang <i@do.codes>
2018-11-18 23:09:44 +0800

    feat: Supporting binding to ref

--- index.js
+++ index.js
@@ -1,83 +1,109 @@
 
-const { objValueStr2AST, objPropStr2AST, objExpression2Str} = require('./utils');
+const {objValueStr2AST, objPropStr2AST, objExpression2Str, log} = require('./utils');
+
+function getExpFromVariableDeclaration(binding) {
+
+}
+
+function getRefExp(node, refName) {
+  const binding = node.scope.bindings[refName];
+  if (!binding) return null;
+  return getExpFromVariableDeclar(binding);
+  debugger;
+  console.log(binding.value);
+  console.log(binding);
+  log(Object.keys(binding));
+}
+
+function getModelExp(node) {
+  // log(node.node.value.expression);
+  const { expression } = node.node.value;
+  switch (expression.type) {
+    case 'Identifier': {
+      const ref = getRefExp(node, expression.name);
+    }
+  }
+}
 
 
 module.exports = function ({types: t}) {
-    let attrName = 'model';
+  let attrName = 'model';
 
-    function JSXAttributeVisitor(node) {
-        if (node.node.name.name === attrName) {
-            let modelStr = objExpression2Str(node.node.value.expression).split('.');
-            if (modelStr[0] !== 'this' || modelStr[1] !== 'state') return;
+  function JSXAttributeVisitor(node) {
+    if (node.node.name.name === attrName) {
+      let modelExp = getModelExp(node);
+      return;
+      let modelStr = objExpression2Str(node.node.value.expression).split('.');
+      if (modelStr[0] !== 'this' || modelStr[1] !== 'state') return;
 
-            modelStr = modelStr.slice(2, modelStr.length).join('.');
-            const stateDeclaration = t.variableDeclaration(
-                'const', [
-                    t.variableDeclarator(
-                        t.identifier('_state'),
-                        t.memberExpression(
-                            t.thisExpression(),
-                            t.identifier('state')
-                        )
-                    )
-                ]
-            );
-            const setStateCall = t.callExpression(
-                t.memberExpression(
-                    t.thisExpression(),
-                    t.identifier('setState')
-                ),
-                [t.objectExpression(
-                    [objPropStr2AST(modelStr, 'e.target.value', t)]
-                )]
-            );
+      modelStr = modelStr.slice(2, modelStr.length).join('.');
+      const stateDeclaration = t.variableDeclaration(
+        'const', [
+          t.variableDeclarator(
+            t.identifier('_state'),
+            t.memberExpression(
+              t.thisExpression(),
+              t.identifier('state')
+            )
+          )
+        ]
+      );
+      const setStateCall = t.callExpression(
+        t.memberExpression(
+          t.thisExpression(),
+          t.identifier('setState')
+        ),
+        [t.objectExpression(
+          [objPropStr2AST(modelStr, 'e.target.value', t)]
+        )]
+      );
 
-            node.node.name.name = 'value';
-            const onChange = node.parent.attributes.filter(attr => (attr && attr.name && attr.name.name) === 'onChange')[0];
-            if (onChange) {
-                const callee = onChange.value.expression;
-                onChange.value = t.JSXExpressionContainer(
-                    t.arrowFunctionExpression(
-                        [t.identifier('e')],
-                        t.blockStatement([
-                            stateDeclaration,
-                            t.expressionStatement(setStateCall),
-                            t.expressionStatement(
-                                t.callExpression(
-                                    callee,
-                                    [t.identifier('e')]
-                                )
-                            )
-                        ])
-                    )
+      node.node.name.name = 'value';
+      const onChange = node.parent.attributes.filter(attr => (attr && attr.name && attr.name.name) === 'onChange')[0];
+      if (onChange) {
+        const callee = onChange.value.expression;
+        onChange.value = t.JSXExpressionContainer(
+          t.arrowFunctionExpression(
+            [t.identifier('e')],
+            t.blockStatement([
+              stateDeclaration,
+              t.expressionStatement(setStateCall),
+              t.expressionStatement(
+                t.callExpression(
+                  callee,
+                  [t.identifier('e')]
                 )
-            } else {
-                node.insertAfter(t.JSXAttribute(
-                    t.jSXIdentifier('onChange'),
-                    t.JSXExpressionContainer(
-                        t.arrowFunctionExpression(
-                            [t.identifier('e')],
-                            t.blockStatement([
-                                stateDeclaration,
-                                t.expressionStatement(setStateCall)
-                            ])
-                        )
-                    )
-                ));
-            }
-        }
+              )
+            ])
+          )
+        )
+      } else {
+        node.insertAfter(t.JSXAttribute(
+          t.jSXIdentifier('onChange'),
+          t.JSXExpressionContainer(
+            t.arrowFunctionExpression(
+              [t.identifier('e')],
+              t.blockStatement([
+                stateDeclaration,
+                t.expressionStatement(setStateCall)
+              ])
+            )
+          )
+        ));
+      }
     }
+  }
 
-    function JSXElementVisitor(path) {
-        attrName = this.opts && this.opts.attrName || attrName;
-        path.traverse({
-            JSXAttribute: JSXAttributeVisitor
-        });
-    }
+  function JSXElementVisitor(path) {
+    attrName = this.opts && this.opts.attrName || attrName;
+    path.traverse({
+      JSXAttribute: JSXAttributeVisitor
+    });
+  }
 
-    return {
-        visitor: {
-            JSXElement: JSXElementVisitor
-        }
+  return {
+    visitor: {
+      JSXElement: JSXElementVisitor
     }
+  }
 };
--- utils/index.js
+++ utils/index.js
@@ -1,75 +1,76 @@
-
-import * as types from './types';
-
-
 function objValueStr2AST(objValueStr, t) {
-    const values = objValueStr.split('.');
-    if (values.length === 1)
-        return t.identifier(values[0]);
-    return t.memberExpression(
-        objValueStr2AST(values.slice(0, values.length - 1).join('.'), t),
-        objValueStr2AST(values[values.length - 1], t)
-    )
+  const values = objValueStr.split('.');
+  if (values.length === 1)
+    return t.identifier(values[0]);
+  return t.memberExpression(
+    objValueStr2AST(values.slice(0, values.length - 1).join('.'), t),
+    objValueStr2AST(values[values.length - 1], t)
+  )
 }
 
 
 function objPropStr2AST(key, value, t) {
-    key = key.split('.');
-    return t.objectProperty(
-        t.identifier(key[0]),
-        key2ObjCall(key, value, t)
+  key = key.split('.');
+  return t.objectProperty(
+    t.identifier(key[0]),
+    key2ObjCall(key, value, t)
+  );
+
+  function key2ObjCall(key, value, t, index) {
+    !index && (index = 0);
+    if (key.length - 1 <= index)
+      return objValueStr2AST(value, t);
+    return t.callExpression(
+      t.memberExpression(
+        t.identifier('Object'),
+        t.identifier('assign')
+      ),
+      [
+        t.objectExpression([]),
+        objValueStr2AST(indexKey2Str(index + 1, key), t),
+        t.objectExpression([
+          t.objectProperty(
+            t.identifier(key[index + 1]),
+            key2ObjCall(key, t, index + 1)
+          )
+        ])
+      ]
     );
 
-    function key2ObjCall(key, value, t, index) {
-        !index && (index = 0);
-        if (key.length - 1 <= index)
-            return objValueStr2AST(value, t);
-        return t.callExpression(
-            t.memberExpression(
-                t.identifier('Object'),
-                t.identifier('assign')
-            ),
-            [
-                t.objectExpression([]),
-                objValueStr2AST(indexKey2Str(index + 1, key), t),
-                t.objectExpression([
-                    t.objectProperty(
-                        t.identifier(key[index + 1]),
-                        key2ObjCall(key, t, index + 1)
-                    )
-                ])
-            ]
-        );
-
-        function indexKey2Str(index, key) {
-            const str = ['_state'];
-            for (let i = 0; i < index; i++) str.push(key[i]);
-            return str.join('.')
-        }
+    function indexKey2Str(index, key) {
+      const str = ['_state'];
+      for (let i = 0; i < index; i++) str.push(key[i]);
+      return str.join('.')
     }
+  }
 }
 
 
 function objExpression2Str(expression) {
-    let objStr;
-    switch (expression.object.type) {
-        case 'MemberExpression':
-            objStr = objExpression2Str(expression.object);
-            break;
-        case 'Identifier':
-            objStr = expression.object.name;
-            break;
-        case 'ThisExpression':
-            objStr = 'this';
-            break;
-    }
-    return objStr + '.' + expression.property.name;
+  let objStr;
+  switch (expression.object.type) {
+    case 'MemberExpression':
+      objStr = objExpression2Str(expression.object);
+      break;
+    case 'Identifier':
+      objStr = expression.object.name;
+      break;
+    case 'ThisExpression':
+      objStr = 'this';
+      break;
+  }
+  return objStr + '.' + expression.property.name;
 }
 
+function log(...args) {
+  args.forEach(i => {
+    console.log(JSON.stringify(i, '', 4));
+  })
+}
 
 module.exports = {
-    objValueStr2AST,
-    objPropStr2AST,
-    objExpression2Str,
-    types
+  objValueStr2AST,
+  objPropStr2AST,
+  objExpression2Str,
+  log
 };
